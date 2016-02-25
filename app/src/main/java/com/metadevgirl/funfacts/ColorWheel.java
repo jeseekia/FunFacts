b5102907bae04614af706255a4334fac36b674ee
Jeseekia Vaughn <jeseekia@gmail.com>
2016-02-24 23:52:05 -0500

    First Commit of project

---
+++ app/src/main/java/com/metadevgirl/funfacts/ColorWheel.java
@@ -0,0 +1,7 @@
+package com.metadevgirl.funfacts;
+
+/**
+ * Created by Jeseekia on 2/15/16.
+ */
+public class ColorWheel {
+}
---
+++ app/src/main/java/com/metadevgirl/funfacts/FunFactsActivity.java
@@ -0,0 +1,39 @@
+package com.metadevgirl.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+    private FactBook mFactBook = new FactBook();
+//  Declare our view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //Assign the Views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(Color.RED);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
